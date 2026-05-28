cf27d8da6b8ad4fd00cbe4f47e8408f17ff62422
hyunbikim <152488051+myeonbi@users.noreply.github.com>
2026-05-28 14:17:19 +0900

    Update 3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, const char* argv[]) {
-    if (argc < 2) 
+    if (argc < 3)
         return 0;
-    
+
     FILE* fp = fopen(argv[1], "r");
+    const char* target_str = argv[2];
 
-
-    return 0;
+    char dst_str[256];
+    while (fgets(dst_str, sizeof(dst_str), fp) != NULL) {
+        if (strstr(dst_str, target_str) != NULL) {
+            printf("%s", dst_str);
+        }
+    }
+    fclose(fp);
 }
