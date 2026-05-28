a36bc4517773c5a1e88471e3b3c6a60eab4754a8
hyunbikim <152488051+myeonbi@users.noreply.github.com>
2026-05-28 14:16:48 +0900

    Update 2-5.c

--- code/2-5.c
+++ code/2-5.c
@@ -4,6 +4,15 @@ int main(int argc, const char* argv[]) {
     if (argc < 3)
         return 0;
 
+    FILE* fp_src = fopen(argv[1], "r");
+    FILE* fp_dst = fopen(argv[2], "w");
 
+    char ch = fgetc(fp_src);
+    while (ch != EOF) {
+        fputc(ch, fp_dst);
+        ch = fgetc(fp_src);
+    }
+
+    fclose(fp_src);
+    fclose(fp_dst);
 }
-
