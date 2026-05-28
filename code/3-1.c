8ecba141fa4c65b7272371a76c08747f7005cdff
hyunbikim <152488051+myeonbi@users.noreply.github.com>
2026-05-28 14:35:44 +0900

    Update 3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
-#include <string.h>
 
 int main(int argc, const char* argv[]) {
-    if (argc < 3)
+    if (argc < 2)
         return 0;
 
     FILE* fp = fopen(argv[1], "r");
-    const char* target_str = argv[2];
-
-    char dst_str[256];
-    while (fgets(dst_str, sizeof(dst_str), fp) != NULL) {
-        if (strstr(dst_str, target_str) != NULL) {
-            printf("%s", dst_str);
-        }
+    if (fp == NULL) {
+        perror("file does not exists");
+        return 1;
     }
-    fclose(fp);
+
+    return 0;
 }
