8877f413b9322a19b748d0168cb9bf063f9e33e3
hyunbikim <152488051+myeonbi@users.noreply.github.com>
2026-05-28 14:16:35 +0900

    Update 2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -6,8 +6,21 @@ int main(int argc, const char* argv[]) {
 
     FILE* fp = fopen(argv[1], "r");
 
+    if (fp == NULL)
+        return 0;
 
+    int count[26] = {0};
+    char ch;
 
+    while ((ch = fgetc(fp)) != EOF) {
+        if (ch >= 'a' && ch <= 'z') {
+            count[ch - 'a'] += 1;
+        }
+    }
+
+    for (int i = 0; i < 26; i++) {
+        printf("%c: %d\n", 'a' + i, count[i]);
+    }
+    
     fclose(fp);
 }
-
