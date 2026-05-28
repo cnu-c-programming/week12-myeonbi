294360dc22cce9a757fe9d437d467c47dbfef90f
hyunbikim <152488051+myeonbi@users.noreply.github.com>
2026-05-28 14:16:08 +0900

    Update 2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -11,12 +11,20 @@ int main(int argc, const char* argv[]) {
     int count = 0;
     Student students[64];
 
-
+    while (fscanf(fp, "%s %d", students[count].name, &students[count].score) == 2) {
+        count++;
+    }
 
     int max = 0;
     float avg = 0;
+    int sum = 0;
+    for (int i = 0; i < count; i++) {
+        if (students[i].score > max)
+            max = students[i].score;
 
-
+        sum += students[i].score;
+    }
+    avg = (float)sum / count;
 
     printf("max: %d\n", max);
     printf("avg: %.2f\n", avg);
@@ -25,4 +33,3 @@ int main(int argc, const char* argv[]) {
 
     return 0;
 }
-
